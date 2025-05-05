ffbdebaa4245eefb399f525cceb72b9e8e3a2dee
Ethan Bao Duc Doan <edoan@terpmail.umd.edu>
2025-05-05 17:32:55 +0000

    Tweaks to Peer struct

--- include/peer_manager.h
+++ include/peer_manager.h
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
 #define MAX_OUTSTANDING_REQUESTS 10                 // Max number of requests "in-flight" (arbitrary number 10, adjust as needed)
 
@@ -9,20 +11,22 @@ struct Peer {
     unsigned char id[20];                           // Hashed peer ID
     
     // Manages if we can upload/download
-    int choking;                                    // Choking status, 1 meaning you are choking this peer (you won't upload to it)
-    int interested;                                 // This peer is interested if we have pieces it doesn't have
+    bool choking;                                   // Choking status, 1 meaning you are choking this peer (you won't upload to it)
+    bool interested;                                // This peer is interested if we have pieces it doesn't have
 
     // Announced by the peer to indicate which pieces it has
     uint8_t *bitfield;                              // Bitmask of pieces this peer has (1 bit -> 1 piece)
-    int bitfield_length;                            // Number of bitfield elements
+    size_t bitfield_length;                         // Number of bitfield elements
 
     // Keep track of outstanding requests to this peer
-    int num_outstanding;                            // Number of outstanding requests (messages in-flight)
-    struct outstanding_request {                    // In-flight request and its fields (kept in queue implemented as a circular array)
+    int num_outstanding_requests;                   // Number of outstanding requests (messages in-flight)
+    struct request {                                // In-flight request and its fields (kept in queue implemented as a circular array)
         uint32_t index;
         uint32_t begin;
         uint32_t length;
     } outstanding_requests[MAX_OUTSTANDING_REQUESTS];
 
-    // Probably will add more to help handle incoming messages (like an input stream buffer)
+    // Buffer for incoming messages per peer to make message parsing easier
+    unsigned char *incoming_buffer;                 // Buffer usage will allow processing incoming messages however they come
+    size_t incoming_buffer_len;
 };
